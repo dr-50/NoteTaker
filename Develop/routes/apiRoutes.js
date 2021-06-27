b1c7ee680a327427d92bad6b23edc04a4abe01ef
Devon <devonross50@yahoo.com>
2021-06-26 20:47:48 -0500

    start of post notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const { json } = require('express');
 const fs = require('fs');
+const { parse } = require('path');
 const router = require('express').Router();
 //const { dbnotes } = require('./db/db.json');
 const util = require('util');
@@ -21,10 +22,46 @@ router.get('/notes', (req, res) => {
 
 });
 
-// router.post('/notes', (req,res) => {
-//     const note = body;
+router.post('/notesa', (req,res) => {
+    //const note = req.body;    
 
-//     return writeFileAsync('../Develop/db/db.json', note)
+    const notesArr =  readFileAsync('./db/db.json', "utf8").then(note => {
+        console.log(note)
+        let parsedNotes;
+
+        parsedNotes=JSON.parse(note);
+        res.json(parsedNotes)
+    }).then(data => {
+        console.log(data);
+        newNotes = req.body;
+        console.log(newNotes);
+        // return writeFileAsync('./db/db.json', newNotes);
+        // return writeFileAsync('./db/db.json', data).then(notes => {
+
+        // })
+    })
+
+    //console.log(notesArr);
+
+
+
+    //console.log(notesArr)
+
+    // return writeFileAsync('./db/db.json', noteArr).then(note => {
+    //     console.log('here')
+
+    //     .then( notes => {
+        
+    //     //console.log(note)
+    //         let parsedNotes;
+
+    //     // parsedNotes = [].concat(JSON.parse(note));
+    //     parsedNotes=JSON.parse(note);
+    //         console.log(parsedNotes)
+    //     res.json(parsedNotes)
+    // })
+})
 // })
+
 
 module.exports = router;
